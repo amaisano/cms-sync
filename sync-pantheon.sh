de46d541e127d61a9e5caa73ffb2e3802d594589
Adamo Maisano <amaisano@mbta.com>
2025-07-22 13:46:10 -0400

    add sandbox to automatic sync (#1)

--- sync-pantheon.sh
+++ sync-pantheon.sh
@@ -25,4 +25,12 @@ terminus env:clone-content -v -y --cc mbta.live test
 echo -e "\nCloning test site to dev..."
 terminus env:clone-content -v -y --cc mbta.test dev
 
-echo -e "\nPantheon test and dev environments have been successfully synced with live."
+# backup sandbox DB prior to sync
+echo -e "\nBacking up sandbox database..."
+terminus backup:create --element=db mbta.sandbox
+
+# clone database and files to sandbox
+echo -e "\nCloning dev site to sandbox..."
+terminus env:clone-content -v -y --cc mbta.dev sandbox
+
+echo -e "\nPantheon test, dev, and sandbox environments have been successfully synced with live."
